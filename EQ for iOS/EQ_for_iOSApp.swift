f6a43e293303fa582f066ce2d06b8e60fbfa0340
FancyGuitarist <simf3010@gmail.com>
2023-01-28 17:20:17 -0500

    Initial Commit

---
+++ EQ for iOS/EQ_for_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  EQ_for_iOSApp.swift
+//  EQ for iOS
+//
+//  Created by Simon Ferland on 2023-01-28.
+//
+
+import SwiftUI
+
+@main
+struct EQ_for_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
